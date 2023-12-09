00b729271590294f0a6a0eac7cb9823e2cfbd987
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2023-12-09 23:47:46 +0300

    added header and start-page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import st from "./Header.module.scss";
-
+import { Link } from "react-router-dom"
 
 const Header:React.FC = () => {
     return(
-        <header>
-            fgdg
+        <header className={st.header}>
+            <div className={st.wrapper_header}>
+                <div className={st.header_home}>home</div>
+                <div className={st.header_buttons}>
+                    <ul className={st.links}>
+                        <li><Link to="/">Your statistic</Link></li>
+                        <li><Link to="/">Your statistic</Link></li>
+                    </ul>
+                </div>
+            </div>
         </header>
     );
 }
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -4,8 +4,20 @@ import st from "./HomePage.module.scss";
 
 const HomePage:React.FC  = () => {
     return(
-        <div className={st.home}>
-
+        <div className={st.homeConteiner}>
+            <div className={st.home}>
+                <aside className={st.home_getStarted}>
+                    <span className={st.home_first_title}>Learn</span>
+                    <span className={st.home_second_title}>English</span>
+                    <span className={st.home_three_title}>effectively</span>
+                    <button className={st.home_bt_gt_st} type="button">Get started</button>
+                </aside>
+                <div className={st.home_image}>
+                    <div className={st.home_picture}>
+                        <img src={"./public/pictures/flagEngland.jpg"} alt="flag" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
